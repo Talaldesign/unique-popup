aec55ec270753e1643a97d05852784936e72e339
Talaldesign <131738738+Talaldesign@users.noreply.github.com>
2025-11-09 00:27:08 +0400

    Refactor supabaseClient initialization method

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // استخدم window.supabase لتجنب ReferenceError
-const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-
+const supabaseClient = Supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 async function loadInitial(){
   const { data: phrasesData } = await supabaseClient.from('phrases').select('id,text').order('id', {ascending:false});
   const phrases = (phrasesData || []).map(r => r.text);
