4a486e46d67f955d6fe4660048107edfdb6e2420
Talaldesign <131738738+Talaldesign@users.noreply.github.com>
2025-11-08 23:29:34 +0400

    Delete src directory

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import PopupShowcase from './PopupShowcase';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <PopupShowcase />
-  </React.StrictMode>
-);
